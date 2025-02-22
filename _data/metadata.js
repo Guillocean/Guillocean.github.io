5a2248c7d7e44e0cfb817ead02750aa80e6b1989
Guillocean <123069091+Guillocean@users.noreply.github.com>
2025-02-22 21:08:50 +0000

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Guillaume's Blog",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a PhD student in Ireland.",
+	author: {
+		name: "Guillaume",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
